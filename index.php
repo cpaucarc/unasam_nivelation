4a7ed9341cb12c528a360ff7d6f1aae3dcbe02bc
Angel Pagola <angelpagolapajuelo99@gmail.com>
2021-02-27 09:32:54 -0500

    html structure in index.html

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
 echo 'v0.1';
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evaluation | UNASAM</title>
+</head>
+<body>
+    
+</body>
+</html>
