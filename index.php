445c97bda352aca4367ecc8fb7e7bb50064e31e0
ANGEL PAGOLA PAJUELO <57104299+angelpagola@users.noreply.github.com>
2021-02-28 18:16:38 -0500

    Delete head in index.php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 require_once "app/views/upperpart.php";
 ?>
-
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Evaluation | UNASAM</title>
-</head>
-<body>
-
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
