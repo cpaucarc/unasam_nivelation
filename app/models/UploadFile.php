6306a44b555bccce3f25bb2f8b9e8362005615b8
ANGEL PAGOLA PAJUELO <57104299+angelpagola@users.noreply.github.com>
2021-03-18 17:56:44 -0500

    Update UploadFile.php

--- app/models/UploadFile.php
+++ app/models/UploadFile.php
@@ -1,4 +1,4 @@
 <?php
 $temporary_file=$_FILES['file']['tmp_name'];
 $name =$_FILES['file']['name'];
-move_uploaded_file($temporary_file,'others/documents/'.$name);
+move_uploaded_file($temporary_file,'../../others/documents/'.$name);
